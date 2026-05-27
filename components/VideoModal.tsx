e44d046844a7ff976326144e0c5835c0481b7a74
Abrar Tahir Sarwar <abrartsarwar@gmail.com>
2026-05-27 14:57:31 -0400

    Revert "perf(video): preload videos and trim modal fade so click feels instant"
    This reverts commit bdb0c464a5a4824d4277b4ac938c4c5db54c5c0b.

--- components/VideoModal.tsx
+++ components/VideoModal.tsx
@@ -82,7 +82,7 @@ export default function VideoModal({
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 0.08 }}
+      transition={{ duration: 0.2 }}
       onClick={onClose}
       role="dialog"
       aria-modal="true"
